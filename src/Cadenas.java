b23d4f73b8acf87d20401f2c22c02c6d5c40cca7
Jose Valenzuela <valenz74@gmail.com>
2022-05-18 21:05:19 -0500

    Aprendiendo Cadenas en Java

---
+++ src/Cadenas.java
@@ -0,0 +1,10 @@
+public class Cadenas {
+    public static void main(String[] args) {
+        String nombre = "Jose";
+        int SeguidoresIG = 5700;
+        int SeguidoresTW = 2100;
+        String frase = "Hola, mi nombre es, " + nombre + ", tengo " + (SeguidoresIG + SeguidoresTW) + " seguidores.";
+        System.out.println(frase);
+
+    }
+}
